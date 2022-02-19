66ee23316908b0e4c01f21f58980ba0e1a4b6d84
Igor Sverchkov <knjaz1989@gmail.com>
2022-02-19 21:56:03 +0300

    req changed

--- stocks_products/entrypoint.sh
+++ stocks_products/entrypoint.sh
@@ -4,5 +4,8 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-exec gunicorn stocks_products.wsgi:application -b 0.0.0.0 --reload
+exec python manage.py runserver
+
+#exec gunicorn stocks_products.wsgi:application -b 0.0.0.0 --reload
+
 
