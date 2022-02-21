577defd128624cd3acfe2037a4882ecfeb60e122
Igor Sverchkov <knjaz1989@gmail.com>
2022-02-21 22:59:40 +0300

    d-c and env changed

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-# import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,5 +140,3 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
-
-# django_heroku.settings(locals())
