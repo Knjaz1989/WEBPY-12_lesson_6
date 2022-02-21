a2a5e5258bc1664657031fb4cd44ef2a4d9817da
Igor Sverchkov <knjaz1989@gmail.com>
2022-02-21 21:58:03 +0300

    ent changed

--- stocks_products/entrypoint.sh
+++ stocks_products/entrypoint.sh
@@ -4,6 +4,6 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-exec gunicorn stocks_products.wsgi:application -b stocks-products-app.herokuapp.com --reload
+exec gunicorn stocks_products.wsgi:application -b 0.0.0.0:80 --reload
 
 
